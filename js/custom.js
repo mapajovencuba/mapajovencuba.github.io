f548bfd441187f6e6499a17d5efb5b92106af6a5
Ing. Dubiel Santana Monzon <dubiels86@gmail.com>
2020-07-28 22:00:09 -0400

    Commit

--- js/custom.js
+++ js/custom.js
@@ -584,6 +584,7 @@ $.getJSON("data/cuba.geojson",
 			$('#profile-title-block').css('background-color',topicData['General'].color);
 			$('#project-name').html(e.nombre);
 			$('#project-category').html(setStrings(e.categoria));
+			$('#project-estructura').html(setStrings(e.estructura));
 			$('#project-reach').html(e.alcance);
 			$('#project-keywords').html(setStrings(e.keywords));
 			$('#project-description').html(e.descripcion);
